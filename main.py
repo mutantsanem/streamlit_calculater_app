ac1d7aaab3fc89ab4be6940cfc5e0ad6be7dd909
mutantsanem <omkrpatil117@gmail.com>
2026-02-12 18:02:29 +0530

    UI improved, delay update fixed

--- main.py
+++ main.py
@@ -13,25 +13,33 @@ st.text_input("Expression", st.session_state["expression"], disabled=True)
 cols = st.columns(4)
 
 buttons = [
-    "7", "8", "9", "plus +",
-    "4", "5", "6", "minus -",
-    "1", "2", "3", "multi *",
-    "0", "C", "=", "divide /"
+    ["7", "8", "9", "➕"],
+    ["4", "5", "6", "➖"],
+    ["1", "2", "3", "✖"],
+    ["0", "C", "=", "/"]
 ]
 
-for i, button in enumerate(buttons):
-    col = cols[i % 4]
-    if col.button(button):
-        if button == "C":
-            st.session_state["expression"] = ""
-        elif button == "=":
-            try:
-                st.session_state["expression"] = str(
-                    eval(st.session_state["expression"])
-                )
-            except:
-                st.session_state["expression"] = "Error"
-        else:
-            st.session_state["expression"] += button
+symbol_map = {
+    "➕": "+",
+    "➖": "-",
+    "✖": "*",
+    "➗": "/"
+}
 
-        st.rerun() 
+for row in buttons:
+    cols = st.columns(4)
+    for i, button in enumerate(row):
+        if cols[i].button(button, use_container_width=True):
+            if button == "C":
+                st.session_state["expression"] = ""
+            elif button == "=":
+                try:
+                    st.session_state["expression"] = str(
+                        eval(st.session_state["expression"])
+                    )
+                except:
+                    st.session_state["expression"] = "Error"
+            else:
+                  st.session_state["expression"] += symbol_map.get(button, button)
+
+            st.rerun()
